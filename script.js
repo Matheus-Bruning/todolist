5dc0865907f53f4f693e130945d982ea8c5a30a3
Matheus Bruning <106267595+Matheus-Bruning@users.noreply.github.com>
2022-06-06 14:09:43 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ let ramos = []
 
 function adicionar(){
   ramos.push(neymar.value)
-  console.log(ramos) 
+  console.log(ramos)
+  render()
+}
+
+function render(){
+  messi.innerHTML = null
+  ramos.forEach(function(tarefa){
+    const li = document.createElement('li')
+    li.innerText = tarefa
+    messi.appendChild(li)
+  })
+}
+function apagar(){
+  ramos = []
+  messi.innerHTML = null
 }
