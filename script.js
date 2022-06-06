8dd22a98b592ba5ac2ec4b76de1dc6184f139272
Matheus Bruning <106267595+Matheus-Bruning@users.noreply.github.com>
2022-06-06 13:35:01 -0300

    .commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const neymar = document.querySelector('input')
+const messi = document.querySelector('ol')
+let ramos = []
+
+function adicionar(){
+  ramos.push(neymar.value)
+  console.log(ramos) 
+}
